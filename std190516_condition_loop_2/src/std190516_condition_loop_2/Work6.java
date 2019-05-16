8d5e10246b22e88172d1d11ef4ad8ac8a59d0494
HyeongJunMin <alsgudwns007@naver.com>
2019-05-16 19:45:04 +0900

    study_stars!

--- std190516_condition_loop_2/src/std190516_condition_loop_2/Work6.java
+++ std190516_condition_loop_2/src/std190516_condition_loop_2/Work6.java
@@ -1,13 +1,14 @@
 package std190516_condition_loop_2;
 
 public class Work6 {
-	void play() {
-		// 1, 3, 5, 7, 9, 7, 5, 3, 1
-		// 4,3,2,1,0 1,2,3,4
-		// 4,5,6,7,8 7,6,5,4
-		int origin = 31;
+	void play1() {
+		// 모래시계 모양으로 별찍기
+		
+		int origin = 11;
 		int a = origin;
 		int b = 0;
+		System.out.println("size: " + origin);
+		System.out.println();
 		
 		for (int i = 0; i < origin; i++) {
 			for (int j = 0; j < b; j++)
@@ -20,6 +21,29 @@ public class Work6 {
 			b = (i<(origin/2))?b+1:b-1;
 			System.out.println();
 		}
+	}
+	void play2() {
+		//속이 비어있는 마름모 모양으로 별 찍기
+		
+		int size = 9;	//행렬 size 설정
+		int x=0,y1=size/2,y2=size/2;	//행렬 x,y좌표
+		char[][] arr = new char[9][9];
+		System.out.println("size: " + size + ", x: " + x + ", y: " + y1);
+		System.out.println();
+		for (int i = 0; i < size; i++) {
+			arr[x][y1] = '*';
+			arr[x][y2] = '*';
+			x++;
+			y1 = ( x > (size/2) )? y1-1 : y1+1;
+			y2 = ( x > (size/2) )? y2+1 : y2-1;
+		}
+		
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) 
+				System.out.print(arr[i][j]);
+			System.out.println();
+		}
+		
 	}
 }
 
