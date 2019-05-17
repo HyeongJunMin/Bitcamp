2dcbdb07b9a1ced445c0c57b43eb16bc2907875a
HyeongJunMin <alsgudwns007@naver.com>
2019-05-17 11:18:24 +0900

    study String, class, method

--- std190517_condition_loop_3/src/std190517_condition_loop_3/Lct1_String.java
+++ std190517_condition_loop_3/src/std190517_condition_loop_3/Lct1_String.java
@@ -27,6 +27,63 @@ public class Lct1_String {
 		char cdata[] = { 'h', 'e', 'l', 'l', 'o' };
 		String str6 = new String(cdata);
 		System.out.println(str6);
+		
+		String str7 = "hello";
+		String str8 = "hello";
+		if(str7 == str8)
+			System.out.println("str7 == str8");
+		else
+			System.out.println("str7 != str8");
+		
+		str8 = "hell";
+		str8 = str8 + "o";
+		
+		if(str7 == str8)	
+			System.out.println("str7 == str8");
+		else				
+			System.out.println("str7 != str8");
+		//	String 변수의 ==연산은 초기화 내용만 비교되기 때문에 잘못된 결과값이 나옴.
+		//	초기화 내용 외에 변수의 현재값을 적용하지 못함
+		
+		if(str7.equals(str8))
+			System.out.println("str7 == str8");
+		else				
+			System.out.println("str7 != str8");
+		//	변수의 현재 값을 비교하기 위해서는 equals method를 사용해야 함.
+		
+		boolean b1 = str7.equals(str8);
+		System.out.println(b1);
+		
+		String str9 = "asdasdasdasd";
+		int n = str9.indexOf("d");	//	문자열의 앞부분 부터 입력받은 문자열 또는 문자 탐색해서 index return
+		System.out.println("str9.indexOf() : " + n);
+		
+		n = str9.lastIndexOf("d");	//	문자열의 끝부분 부터 입력받은 문자열 또는 문자 탐색해서 index return
+		System.out.println("str9.lastIndexOf() : " + n);
+		
+		int len = str9.length();	//	문자열의 길이 return
+		System.out.println("str9.length() : " + len);
+		
+		String str10 = "A*B*C*D";	
+		String repStr = str10.replace("*", "-");	//	변수의 인수1을 인수2로 수정
+		System.out.println("str10.replace(\"*\", \"-\") : " + repStr);
+		str10 = "형용사형 methodless | methodical";
+		repStr = str10.replace("|", "");
+		System.out.println("str10.replace(\"|\", \"\") : " + repStr);
+		
+		//	split == token을 이용해서 문자열을 자르는 작업을 수행
+		//	token은 뭡니까? 구분자입니다.
+		String str11 = "홍길동-24-2001/01/13-서울시";
+		String splStr11[] = str11.split("-");
+		for(String a : splStr11)
+			System.out.print(a + " ");
+		System.out.println();
 	}
 
 }
+
+
+
+
+
+//공백
--- std190517_condition_loop_3/src/std190517_condition_loop_3/Main.java
+++ std190517_condition_loop_3/src/std190517_condition_loop_3/Main.java
@@ -2,6 +2,7 @@ package std190517_condition_loop_3;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("2019-05-17 강의 내용입니다.");
 //		Review_190516 r1 = new Review_190516();
 //		r1.play2();
 		Lct1_String l1 = new Lct1_String();
