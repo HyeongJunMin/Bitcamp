42215e64a454fc36634d2e19250ab0c5bb390ca0
HyeongJunMin <alsgudwns007@naver.com>
2019-05-20 10:49:53 +0900

    study two-dimensional-array, wrapper class

---
+++ std190520_/src/std190520_twodmnarr_wrapperclass/Main190520.java
@@ -0,0 +1,11 @@
+package std190520_twodmnarr_wrapperclass;
+
+public class Main190520 {
+	public static void main(String[] args) {
+		//	1. 5/17 내용 복습(조건과 반복)
+		//	2. 2차원 배열
+		//	3. wrapper class
+		Review190517 r = new Review190517();
+		r.play4();
+	}
+}
