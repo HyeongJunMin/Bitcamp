cc7bc10a3e3f6261dcb736dbe475054ec2f26c69
HyeongJunMin <alsgudwns007@naver.com>
2019-05-24 21:17:24 +0900

    study java method3

--- std190524_method3/src/std190524_method3/Main.java
+++ std190524_method3/src/std190524_method3/Main.java
@@ -5,7 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		String a = "12345";
 		
+		System.out.println(a.contains("6"));
 		
 		
 		
